8241adf92ac870c9eeb649e6e51618fd7bf419a5
Aamir <aamirhafiez@gmail.com>
2020-08-08 03:34:04 +0530

    setup template engine ejs and created and rendered home.ejs

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 
 app.use('/', require('./routes'));
 
+//setting up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`OOPs!! Error in running server: ${err}`);
