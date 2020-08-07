1bc1525537af8b438f0d5fb05ab3efac2e2d8f60
Aamir <aamirhafiez@gmail.com>
2020-08-08 02:38:45 +0530

    added a controller and a router for users

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const homeController = require('../controllers/home_controller');
 
 console.log('Yipee!! Router is loaded');
 
+router.use('/users', require('./users.js'));
 router.get('/', homeController.home);
 
 module.exports = router;
