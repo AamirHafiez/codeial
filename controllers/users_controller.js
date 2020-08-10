98a702cee35f3bb6552db07f743abd1adb40ce2f
Aamir <aamirhafiez@gmail.com>
2020-08-10 20:40:45 +0530

    created forms for sign up and sign in

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = function(req, res){
         title: 'codeial | profile'
     });
 }
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in.ejs', {
+        title: 'codeial | sign in'
+    });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up.ejs', {
+        title: 'codeial | sign up'
+    });
+}
+
+module.exports.create = function(req, res){
+
+}
+
+module.exports.createSession = function(req, res){
+
+}
